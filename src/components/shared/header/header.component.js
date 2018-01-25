399da133c98a8963e345d3b2a259754f86204719
Rubens Ten <rubens.prl10@gmail.com>
2018-01-24 23:27:38 -0600

    Adding shared component and sass libs

---
+++ src/components/shared/header/header.component.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class HeaderComponent extends Component {
+    state = {}
+    render() { 
+        return ( <div>Heder component</div> )
+    }
+}
+ 
+export default HeaderComponent;
